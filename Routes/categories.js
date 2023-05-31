957a85aa3c6c22d2934928cc83e03e85a490a95e
Pooja <poojasivakumar97@gmail.com>
2023-05-31 17:46:55 +0530

    Feat: backend and mongodb integration

--- Routes/categories.js
+++ Routes/categories.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const router = express.Router();
+const mongoose = require("mongoose");
 const Joi = require("joi");
+const router = express.Router();
 
-let categories = [
-  { id: 1, name: "Web development" },
-  { id: 2, name: "Mobile App development" },
-  { id: 3, name: "photography" },
-];
+// Mongoose schema - Categories
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 40 },
+});
+// Mongodb model
+const Category = new mongoose.model("category", categorySchema);
 
 // get all categories
-router.get("/categories", (req, res) => {
+router.get("/", async (req, res) => {
+  let categories = await Category.find();
   res.status(200).send(categories);
 });
 
 // get one  category
-router.get("/categories/:id", (req, res) => {
-  const category = categories.find((c) => c.id == req.params.id);
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  const category = await Category.findById(id);
   if (!category) {
     res.status(404).status("not found");
   }
@@ -29,38 +33,44 @@ function validateData(category) {
   return Joi.validate(category, schema);
 }
 // add category
-router.post("/categories", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateData(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
   }
-  const category = {
-    id: categories.length + 1,
+  const category = new Category({
     name: req.body.name,
-  };
-  categories.push(category);
+  });
+  await category.save();
   res.send(category);
 });
 
 // update category
-router.put("/categories/:id", (req, res) => {
-  let category = categories.find((c) => c.id == req.params.id);
+router.put("/:id", async (req, res) => {
+  let { error } = validateData(req.body);
+  if (error) {
+    res.status(404).send(error.details[0].message);
+  }
+  let category = await Category.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
   if (!category) {
     res.status(404).send("category not found!");
   }
-  category.name = req.body.name;
   res.send(category);
 });
 
 // delete category
 
-router.delete("/categories/:id", (req, res) => {
-  let category = categories.find((c) => c.id == req.params.id);
+router.delete("/:id", async (req, res) => {
+  let category = await Category.findByIdAndRemove(req.params.id);
   if (!category) {
     res.status(404).send("category not found!");
   }
-  let index = categories.indexOf(category);
-  categories.splice(index, 1);
+  // let index = categories.indexOf(category);
+  // categories.splice(index, 1);
   res.status(204).send("category deleted!");
 });
 
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const categories = require("./Routes/categories");
 const PORT = 8080 || process.env.PORT;
 
+// MongoDb connection
+mongoose
+  .connect("mongodb://127.0.0.1/e-learning")
+  .then((res) => {
+    console.log("Connected with DB Successfully !");
+  })
+  .catch((e) => {
+    console.log(`Couldn't connect with db`);
+  });
+
 app.use(express.json());
-app.use(categories);
+app.use("/categories", categories);
 
 app.get("/", (req, res) => {
   res.status(200).send("Home");
